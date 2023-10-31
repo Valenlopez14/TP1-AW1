0f5f9e808a7e332cfef52ac3ea918b352e691443
root <mgiraudo@ies21.edu.ar>
2023-10-31 10:47:53 -0300

    asd

---
+++ productos.js
@@ -0,0 +1,21 @@
+const productos = {
+    nombre : "Remeras", 
+    articulos:[
+        {
+            //Todos las caracteristicas de la remera (precio, talle, marca, descripcion)
+            nombre: "Remera Jordan",
+            tipo: "moda",
+            precio: 15999,
+            imagen: "./imagenes/remera1",
+            link: "https://nike.com",
+            talle: [{talles: "S", stock: 10},
+            {talles: "M", stock:5} ,{talles: "L", stock: 7}
+        ],
+            
+        }
+    ]
+}
+
+
+
+export default productos
